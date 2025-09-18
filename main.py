8ce4ad8d97eb3fcc213961c04574840a537f91e3
RealDianRick <RealDianRick@gmail.com>
2025-09-18 10:05:29 -0300

    ?

---
+++ main.py
@@ -0,0 +1,11 @@
+def trataNumero():
+	try:
+		entradaNumero = int(input("Digite um número: "))
+		print(entradaNumero)
+		
+	except ValueError:
+		print("Burro")
+trataNumero()
+
+
+
--- studies.py
+++ studies.py
@@ -1,1 +1,7 @@
-print("High King")
+code = 10
+salary = 1500.00
+nome = 'José'
+situation = True 
+type0 = type (salary)
+print(salary)
+print(type0)
