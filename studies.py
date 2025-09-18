cca0ba62a7f663d2a89cf2d33754926dffbd519c
RealDianRick <RealDianRick@gmail.com>
2025-09-18 15:30:08 -0300

    Testing

--- studies.py
+++ studies.py
@@ -1,4 +1,3 @@
-codigo = 10
-salario = 1500.00
-nome = 'Jose'
-print("Código: " + str(codigo) + " Nome: " + nome + " O salário atual é de: " + str(salario))
+idade = int (input("Digite a idade da pesso: "))
+if idade > 18:
+    print("Maior de idade.")
