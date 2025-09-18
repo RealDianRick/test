9b13c633af1958cd9e344a71c9261ceceaeae425
RealDianRick <RealDianRick@gmail.com>
2025-09-18 18:02:16 -0300

    Test

--- studies.py
+++ studies.py
@@ -1,3 +1,11 @@
-idade = int (input("Digite a idade da pesso: "))
-if idade > 18:
-    print("Maior de idade.")
+idade = int(input("Digite sua idade: "))
+if idade <= 1:
+    print("Você é um bebê")
+elif idade <= 9:
+    print("Você é criança")
+elif idade <= 17:
+    print("Você é adolescente")
+elif idade <= 59:
+    print("Você é adulto")
+else:
+    print("Você é idoso")
